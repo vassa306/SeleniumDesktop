d172f4dbade4a17d781b986ba75eb6a4370330c2
Václav Juřena <vassa306@gmail.com>
2025-01-26 00:21:21 +0100

    fix verifyStatusBar and add Save file text

--- src/test/java/TestCalc.java
+++ src/test/java/TestCalc.java
@@ -6,9 +6,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
+import static org.openqa.selenium.By.*;
+
 public class TestCalc extends BaseTests {
+
 
 
     @Test
@@ -20,8 +27,11 @@ public class TestCalc extends BaseTests {
         driver.findElementByName("View").click();
 
         // Locate the "Status Bar" menu item
-        WebElement statusBarMenuItem = driver.findElement(By.xpath("//MenuItem[@Name='Status Bar']"));
-
+        WebElement statusBarMenuItem = driver.findElement(xpath("//MenuItem[@Name='Status Bar']"));
+        String state = statusBarMenuItem.getAttribute("LegacyState");
+        if (state.contains("0")) {
+            System.out.println("Element is checked " + state);
+        }
         statusBarMenuItem.click();
 
 
@@ -31,15 +41,21 @@ public class TestCalc extends BaseTests {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         boolean isStatusBarDisplayed;
         try {
-            WebElement statusBarElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("msctls_statusbar32")));
+            // Wait for the status bar element to become visible
+            WebElement statusBarElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("msctls_statusbar32")));
             isStatusBarDisplayed = statusBarElement.isDisplayed();
+            System.out.println("Status bar is displayed" + isStatusBarDisplayed);
+            // Assert true if the element is displayed
+            Assert.assertTrue(isStatusBarDisplayed, "Status bar is not displayed but was expected to be.");
         } catch (TimeoutException e) {
-            isStatusBarDisplayed = false; // Element not found, implying it's not displayed
+            isStatusBarDisplayed = false;
+            // Assert false if the element is not found within the timeout
+            Assert.assertFalse(isStatusBarDisplayed, "Status bar is displayed but was expected not to be.");
         }
 
         // Verify that the state of the status bar matches the menu item's state
         System.out.println(isStatusBarDisplayed);
-        Assert.assertFalse(isStatusBarDisplayed, "Expected Status Bar to be hidden, but it is displayed.");
+
 
     }
 
@@ -48,7 +64,7 @@ public class TestCalc extends BaseTests {
 
         driver.findElementByName("Help").click();
         driver.findElementByName("About Notepad").click();
-        WebElement aboutNotepadElement = driver.findElement(By.name("About Notepad"));
+        WebElement aboutNotepadElement = driver.findElement(name("About Notepad"));
         // Retrieve the value of the "Value.Value" attribute
         String value = aboutNotepadElement.getText();
         Assert.assertEquals(value, "About Notepad", "Actual string is not expected " + value);
@@ -60,7 +76,7 @@ public class TestCalc extends BaseTests {
     public void closeAboutNotepad() {
         driver.findElementByName("Help").click();
         driver.findElementByName("About Notepad").click();
-        WebElement closeButton = driver.findElement(By.xpath("//Window[@ClassName='#32770']//Button[@Name='Close']"));
+        WebElement closeButton = driver.findElement(xpath("//Window[@ClassName='#32770']//Button[@Name='Close']"));
         // Retrieve the value of the "Value.Value" attribute
         boolean clsIsDispl = closeButton.isDisplayed();
         Assert.assertTrue(clsIsDispl, "Close button is missing in the About notepad dialog window");
@@ -69,15 +85,38 @@ public class TestCalc extends BaseTests {
     }
 
     @Test
-    public void loadFile() throws InterruptedException {
+    public void saveFile() throws InterruptedException {
         //click on File menu
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.findElementByName("Text Editor").sendKeys("automation by WinAppDriver");
         driver.findElementByName("File").click();
-        driver.findElementByName("Open...").click();
-        String filePath = "C:\\DesktopApp\\TestNotepad.txt";
-        Thread.sleep(2000);
-        WebElement openFileNameField = driver.findElement(By.className("Edit"));
-        openFileNameField.sendKeys(filePath);
+        WebElement saveAS = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//MenuItem[@AutomationId='4']")));
+        saveAS.click();
+        // Generate the filename with counter and current time
+        String randomString = generateRandomString(6);
+        String fileName = "Test_" + randomString;
+
+        WebElement input = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//Edit[@AutomationId='1001']")));
+        input.sendKeys(fileName);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(name("Save"))).click();
+//        Thread.sleep(4000);
+//        WebElement titlebar = driver.findElementByAccessibilityId("TitleBar");
+//        String actualTitle =  titlebar.getText();
+//        Assert.assertEquals(actualTitle,fileName,"This filenames are different " + fileName);
+
     }
+
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder randomString = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            randomString.append(characters.charAt(index));
+        }
+        return randomString.toString();
+    }
+
 }
 
 
